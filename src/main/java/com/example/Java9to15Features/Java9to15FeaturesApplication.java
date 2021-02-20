845a6d933e01e1f4f0e26a73437d168986233b4b
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2021-02-20 15:25:31 +0530

    second commit

--- src/main/java/com/example/Java9to15Features/Java9to15FeaturesApplication.java
+++ src/main/java/com/example/Java9to15Features/Java9to15FeaturesApplication.java
@@ -1,13 +1,20 @@
 package com.example.Java9to15Features;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Java9to15FeaturesApplication {
+public class Java9to15FeaturesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Java9to15FeaturesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(" ".isEmpty());
+
+
+	}
 }
