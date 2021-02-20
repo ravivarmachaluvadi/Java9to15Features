7dfdfd63385bddbfbe21de1725ef1e5ecf547bf3
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2021-02-20 15:21:34 +0530

    first commit

---
+++ src/main/java/com/example/Java9to15Features/Java9to15FeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java9to15Features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java9to15FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java9to15FeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Java9to15Features/Java9to15FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java9to15Features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java9to15FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
