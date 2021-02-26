c1706c2b6459e8670afc77cb2d53479d6cc1d220
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2021-02-26 22:37:50 +0530

    26 Feb 2021

--- src/main/java/com/example/Java9to15Features/Java9to15FeaturesApplication.java
+++ src/main/java/com/example/Java9to15Features/Java9to15FeaturesApplication.java
@@ -3,17 +3,31 @@ package com.example.Java9to15Features;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Java9to15FeaturesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Java9to15FeaturesApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(Java9to15FeaturesApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(" ".isEmpty());
+		System.out.println(" ".isEmpty()); //false
+		System.out.println(" ".isBlank()); //true
+
+		Stream.of(1,2,3,4,5,6,7,8,9,10).takeWhile(i -> i < 5)
+				.forEach(System.out::println);
+
+		String str = "JD" +
+				"JD" +
+				"JD";
+		System.out.println(str);
+		System.out.println(str.lines().collect(Collectors.toList()));
 
 
 	}
